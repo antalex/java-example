6e9f5ba981f5c25b5ad577a56a43f763f85039ae
Alexander_Muravyev <Alexandr.Muravyev@gmail.com>
2022-02-28 22:43:55 +0400

    Initial commit

---
+++ src/com/ssu/alexander_muravyev/lesson_01/task_01/Application.java
@@ -0,0 +1,14 @@
+package com.ssu.alexander_muravyev.lesson_01.task_01;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Application app = new Application();
+        app.startApplication();
+    }
+    public void startApplication() {
+        System.out.println("App is started");
+        // Your code is here
+    }
+
+}
